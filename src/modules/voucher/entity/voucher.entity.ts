64be961f3fd67d08a00451c26e123eed9d9792f8
Chi Dung <chidung091@gmail.com>
2022-05-18 22:09:49 +0700

    update new

--- src/modules/voucher/entity/voucher.entity.ts
+++ src/modules/voucher/entity/voucher.entity.ts
@@ -37,6 +37,7 @@ export class Vouchers {
   })
   status: boolean
 }
+
 export const VouchersSchema = SchemaFactory.createForClass(Vouchers)
 
 export interface VouchersDocument extends Vouchers, Document {}
